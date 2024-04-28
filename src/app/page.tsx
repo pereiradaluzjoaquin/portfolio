f6234ebd60feca1ee5c7452984f605f1c1e416a4
Joaquin Pereira Da Luz <joaquin.pereiradaluz.ext@bayer.com>
2024-04-28 14:36:43 -0300

    add pages links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,32 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <div className={styles.linksDetail}>
+        <a href="https://hotel-reservation-twelve.vercel.app/">
+          <Image
+            src="/hotelreservationscreen.png"
+            alt="Image"
+            width={500}
+            height={500}
+            layout="responsive"
+          />
+        </a>
+        <p>Hotel Room Reservation</p>
+      </div>
+      <div className={styles.linksDetail}>
+        <a href="https://ecommerce-dun-two.vercel.app/">
+          <Image
+            src="/ecommercescreen.png"
+            alt="Image"
+            width={500}
+            height={500}
+            layout="responsive"
+          />
+        </a>
+        <p>Ecommerce with Fake Api</p>
+      </div>
+    </main>
+  );
 }
